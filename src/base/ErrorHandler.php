773f54942accca4bacaa0ad16598a0a2acca153c
Michał Taglewski <michal@mantisoft.pl>
2022-06-08 10:14:30 +0200

    ErrorHandler very basic version

--- src/base/ErrorHandler.php
+++ src/base/ErrorHandler.php
@@ -7,4 +7,12 @@ namespace mgine\base;
  */
 class ErrorHandler extends Component {
 
+    public function init()
+    {
+        if(!DEBUG_MODE){
+            ini_set('display_errors', 0);
+            ini_set('display_startup_errors', 1);
+            error_reporting(0);
+        }
+    }
 }
